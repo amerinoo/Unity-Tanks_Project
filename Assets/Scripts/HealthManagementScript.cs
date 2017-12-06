1a094ba3880af733b931fa75f25206262b1ea6f6
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-12-06 17:28:50 +0100

    Created fire and generic material for tanks

--- Assets/Scripts/HealthManagementScript.cs
+++ Assets/Scripts/HealthManagementScript.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class HealthManagementScript : MonoBehaviour
 {
 	public float health;
+	public int index;
 	private float maxHealth;
+	public bool h, d;
 
 	private HudControllerScript hcs;
 	// Use this for initialization
@@ -21,6 +23,15 @@ public class HealthManagementScript : MonoBehaviour
 		if (hcs != null) {
 			hcs.UpdateHUD (health);
 		}
+		if (h) {
+			IncrementHealth (100);
+			h = false;
+		}
+		if (d) {
+			ApplyDamage (100);
+			d = false;
+		}
+
 	}
 
 	public void IncrementHealth (int howManyHealth)
@@ -28,13 +39,25 @@ public class HealthManagementScript : MonoBehaviour
 		health += howManyHealth;
 		if (health > maxHealth)
 			health = maxHealth;
+		CheckFire ();
 	}
 
 	public void ApplyDamage (float damage)
 	{
 		health -= damage;
 		if (health <= 0.0f) {
+			GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameControllerScript> ().PlayerDead (index);
 			Destroy (transform.gameObject);
+		}
+		CheckFire ();
+	}
+
+	private void CheckFire ()
+	{
+		int percentaje = Mathf.RoundToInt (health * 100 / maxHealth);
+		int max = transform.Find ("FireZones").childCount;
+		for (int i = 0; i < max; i++) {
+			transform.Find ("FireZones").GetChild (i).gameObject.SetActive (percentaje <= 100 / (max + 1) * (max - i));
 		}
 	}
 }
