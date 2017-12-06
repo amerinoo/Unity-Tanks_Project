e689a2222c757410ec28702c6848e0de9b13bcb6
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-12-06 17:32:42 +0100

    Added Github link in the credits

--- Assets/Scripts/StaticData.cs
+++ Assets/Scripts/StaticData.cs
@@ -30,10 +30,15 @@ public class StaticData : MonoBehaviour
 		"Albert Eduard Merino Pulido\n\n\n\n" +
 		"***Artists (Blender) and animations***\n" +
 		"Albert Eduard Merino Pulido\n\n\n\n" +
+		"***Fonts***\n" +
+		"https://www.dafont.com/es/comical_cartoon.font\n\n\n\n" +
 		"***Music***\n" +
 		"https://www.dl-sounds.com/royalty-free/platformer2/\n\n\n\n" +
 		"***Professors***\n" +
 		"Francisco Sebe Feixas\n\n\n\n" +
+		"***Author***\n" +
+		"Albert Eduard Merino Pulido\n" +
+		"https://github.com/amerinoo/Unity-Tanks-Project\n\n\n\n" +
 		"***Subject information***\n" +
 		"Video game development for high performance platforms\n" +
 		"Academic year 2017-18\n" +
